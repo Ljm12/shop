2c06f7db8d8b06e74bfbb445d1738e297786e9c8
L_JM <l>
2018-10-24 17:48:11 +0800

    indexSwiper

--- src/views/home/components/Header.vue
+++ src/views/home/components/Header.vue
@@ -34,6 +34,8 @@ export default {
         top 0
         left 0
         right 0
+        z-index 100
+        background #fff
         .return-icon
             font-size px2rem(20)
             color  #999
---
+++ src/views/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption" ref="mySwiper" >
+            <!-- slides -->
+            <swiper-slide><img src="//oss.static.nubia.cn/blockimage/153992932088.jpg" alt=""></swiper-slide>
+            <swiper-slide><img src="//oss.static.nubia.cn/blockimage/153933635050.jpg" alt=""></swiper-slide>
+            <swiper-slide><img src="//oss.static.nubia.cn/blockimage/153785721420.jpg" alt=""></swiper-slide>
+            <!-- Optional controls -->
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name:'HomeSweiper',
+    data () {
+        return {
+             swiperOption: {
+                pagination:'.swiper-pagination',
+                loop:true,
+                autoplay:3000
+            }
+        }
+    },
+    computed: {
+         swiper() {
+            return this.$refs.mySwiper.swiper
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/style/global.styl'
+@import '~@/assets/style/varibles.styl'
+    .wrapper>>>.swiper-pagination-bullet-active
+        background-color #fff
+    .wrapper
+        margin-top: $headerHeight
+        width 100%
+        height 0
+        padding-bottom 42%
+        overflow hidden
+</style>
+
