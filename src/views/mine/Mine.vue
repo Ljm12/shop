6c58cbdd97e707d27a1667d30d6b2f130e20829b
L_JM <l>
2018-11-01 10:12:39 +0800

    个人中心+

--- src/views/mine/Mine.vue
+++ src/views/mine/Mine.vue
@@ -2,18 +2,20 @@
     <div >
         <mine-header></mine-header>
         <mine-orders></mine-orders>
-        
+        <mine-list></mine-list>
     </div>        
 </template>
 
 <script>
 import MineHeader from "./components/Header"
 import MineOrders from "./components/Orders"
+import MineList from "./components/List"
 export default {
     name:"Mine",
     components: {
         MineHeader,
-        MineOrders
+        MineOrders,
+        MineList
     }
 }
 </script>
---
+++ src/views/mine/components/List.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="jg"></div>
+        <div class="item">
+            <div class="left">我的资产</div>
+            <div class="right iconfont">&#xe62d;</div>
+        </div>
+        <div class="item">
+            <div class="left">提现记录</div>
+            <div class=" iconfont right">&#xe62d;</div>
+        </div>
+    </div>
+    
+    
+</template>
+
+<script>
+export default {
+    name:'MineList'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/style/global.styl'
+    .jg
+        height px2rem(10)
+        background #f7f7f7
+        
+    .item
+        display flex
+        justify-content space-between
+        padding px2rem(0) px2rem(18)
+        font-size px2rem(14)
+        line-height px2rem(40)
+        border-bottom 1px solid #f2f2f2
+        .right
+            font-size px2rem(14) !important
+            color #999
+
+</style>
