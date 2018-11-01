9dfc8e3c7c9fc86ba7ea27a3a6f2d48908c37e66
L_JM <l>
2018-11-01 15:11:02 +0800

    index ajax

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -17,6 +17,7 @@ import HomeSwiper from './components/Swiper'
 import HomePopular from './components/Popular'
 import HomeFind from './components/Find'
 import HomeFeatured from './components/Featured'
+import axios from 'axios'
 export default {
     components: {
         HomeHeader,
@@ -27,67 +28,26 @@ export default {
     },
     data () {
         return {
-            hotProducts:[{
-                id:'001',
-                imgurl:'//gw3.alicdn.com/bao/uploaded/i3/1917047079/TB1RxS.XpzqK1RjSZFzXXXjrpXa_!!0-item_pic.jpg_210x210.jpg',
-                productName:'ipone 8 plus 64G ipone 8 plus 64G',
-                price:9999
-            },{
-                id:'002',
-                imgurl:'//gw3.alicdn.com/bao/uploaded/i3/1917047079/TB1RxS.XpzqK1RjSZFzXXXjrpXa_!!0-item_pic.jpg_210x210.jpg',
-                 productName:'ipone 8 plus 64G',
-                price:9999
-            },{
-                id:'003',
-                imgurl:'//gw3.alicdn.com/bao/uploaded/i3/1917047079/TB1RxS.XpzqK1RjSZFzXXXjrpXa_!!0-item_pic.jpg_210x210.jpg',
-                productName:'ipone 8 plus 64G',
-                price:9999
-            },{
-                id:'004',
-                imgurl:'//gw1.alicdn.com/bao/uploaded/i1/2616970884/O1CN011IOuaD1Qu6P1Er3_!!0-item_pic.jpg_210x210.jpg',
-                productName:'ipone 8 plus 128G',
-                price:9999
-            },{
-                id:'005',
-                imgurl:'//gw3.alicdn.com/bao/uploaded/i3/1917047079/TB1RxS.XpzqK1RjSZFzXXXjrpXa_!!0-item_pic.jpg_210x210.jpg',
-                productName:'ipone 8 plus',
-                price:9999
-            },{
-                id:'006',
-                imgurl:'//gw1.alicdn.com/bao/uploaded/i1/2616970884/O1CN011IOuaD1Qu6P1Er3_!!0-item_pic.jpg_210x210.jpg',
-                productName:'ipone 8 plus',
-                price:9999
-            }],
-            bannerImg:[
-                '//oss.static.nubia.cn/blockimage/153992932088.jpg',
-                '//oss.static.nubia.cn/blockimage/153933635050.jpg',
-                '//oss.static.nubia.cn/blockimage/153785721420.jpg'
-            ],
-             productsList:[{
-                id:"001",
-                imgurl:"//img10.360buyimg.com/n7/jfs/t20140/279/2633113298/113707/57d9da77/5b6018c5N6f80495e.jpg!q70.dpg",
-                productName:"荣耀Note10 全网通6G+64G 幻夜黑 移动联通电信4G全面屏手机 双卡双待 游戏手机",
-                price:2799
-            },{
-                id:"002",
-                imgurl:"//img10.360buyimg.com/n7/jfs/t20140/279/2633113298/113707/57d9da77/5b6018c5N6f80495e.jpg!q70.dpg",
-                productName:"荣耀Note10 全网通6G+64G 幻夜黑 移动联通电信4G全面屏手机 双卡双待 游戏手机",
-                price:2799
-            },{
-                id:"003",
-                imgurl:"//img10.360buyimg.com/n7/jfs/t20140/279/2633113298/113707/57d9da77/5b6018c5N6f80495e.jpg!q70.dpg",
-                productName:"荣耀Note10 全网通6G+64G 幻夜黑 移动联通电信4G全面屏手机 双卡双待 游戏手机",
-                price:2799
-            },{
-                id:"004",
-                imgurl:"//img10.360buyimg.com/n7/jfs/t20140/279/2633113298/113707/57d9da77/5b6018c5N6f80495e.jpg!q70.dpg",
-                productName:"荣耀Note10 全网通6G+64G 幻夜黑 移动联通电信4G全面屏手机 双卡双待 游戏手机",
-                price:2799
-            }]
+            hotProducts:[],
+            bannerImg:[],
+             productsList:[]
         }
+    },
+    methods: {
+        getHomeInfo () {
+            axios.get('/mock/index.json').then( res =>{
+                var data = res.data
+                this.bannerImg=data.bannerImg
+                this.hotProducts=data.hotProducts
+                this.productsList=data.productsList
+            })
+        }  
     },
     mounted () {
        
+    },
+    created () {
+        this.getHomeInfo()
     }
 }
 </script>
